3012c16d5b398a06cfa0fa643783cd9e1504b553
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-28 00:20:55 +0300

    fix: fix blog controllers

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -8,5 +8,5 @@ router.patch('/blog/update/:id',blogControllers.patchBlog)
 router.delete('/blog/delete/:id',blogControllers.deleteBlog)
 router.get('/blog/getById/:id',blogControllers.getById)
 router.get('/blog/myBlogs',blogControllers.getMyBlog)
-router.get('/blog/userBlogs/:userId',blogControllers.getUserBlog)
+router.get('/blog/userBlogs/:username',blogControllers.getUserBlog)
 export default router
