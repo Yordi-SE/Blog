49cc399e9d7cde15b010862bc6461f99083c616b
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-27 22:59:16 +0300

    fix blog controller logics

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -3,9 +3,10 @@ import blogControllers from "../controller/blogController";
 const router = Router();
 
 router.post('/blog/create',blogControllers.createBlog)
-router.get('/',blogControllers.getAll)
+router.get('/blog',blogControllers.getAll)
 router.patch('/blog/update/:id',blogControllers.patchBlog)
 router.delete('/blog/delete/:id',blogControllers.deleteBlog)
 router.get('/blog/getById/:id',blogControllers.getById)
 router.get('/blog/myBlogs',blogControllers.getMyBlog)
+router.get('/blog/userBlogs/:userId',blogControllers.getUserBlog)
 export default router
