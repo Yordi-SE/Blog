5e54e59decb3e432a5dabde70c31804fddb70700
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-28 13:50:08 +0300

    fix: routers endpoint

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -3,11 +3,11 @@ import blogControllers from "../controller/blogController";
 import validateBlogData from "../middleware/joiBlogValidation";
 const router = Router();
 
-router.post('/blog/create',validateBlogData,blogControllers.createBlog)
-router.get('/blog',validateBlogData,blogControllers.getAll)
-router.patch('/blog/update/:id',validateBlogData,blogControllers.patchBlog)
-router.delete('/blog/delete/:id',validateBlogData,blogControllers.deleteBlog)
-router.get('/blog/getById/:id',validateBlogData,blogControllers.getById)
-router.get('/blog/myBlogs',validateBlogData,blogControllers.getMyBlog)
-router.get('/blog/userBlogs/:username',validateBlogData,blogControllers.getUserBlog)
+router.post('/create',validateBlogData,blogControllers.createBlog)
+router.get('/',validateBlogData,blogControllers.getAll)
+router.patch('/update/:id',validateBlogData,blogControllers.patchBlog)
+router.delete('/delete/:id',validateBlogData,blogControllers.deleteBlog)
+router.get('/getById/:id',validateBlogData,blogControllers.getById)
+router.get('/myBlogs',validateBlogData,blogControllers.getMyBlog)
+router.get('/userBlogs/:username',validateBlogData,blogControllers.getUserBlog)
 export default router
