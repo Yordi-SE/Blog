30a624d76e4cdb98291ba033d7e31c8342ed9914
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-28 14:04:16 +0300

    add: user route to app server

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import tagRoutes from '../routes/tagRoutes';
 import blogRoutes from '../routes/blogRoutes'
+import userRoutes from '../routes/userRoutes';
 const createApp = ():express.Application =>{
     const app:express.Application = express()
     app.use(express.json())
+    app.use('/api/user',userRoutes)
     app.use('/api/tag', tagRoutes)
     app.use('/api/blog',blogRoutes)
     return app
