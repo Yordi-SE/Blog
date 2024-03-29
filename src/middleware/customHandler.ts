b912c895787ff64e942c366c3705500f11b21dc2
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-29 15:39:46 +0300

    add joi blog  validation

--- src/middleware/customHandler.ts
+++ src/middleware/customHandler.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CustomError } from '../../error/customErrors';
+import { CustomError } from '../error/customErrors';
 export default (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     if(err instanceof CustomError) {
       const { statusCode, errors } = err;
